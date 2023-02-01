67061df3b786a91a8458902499418d1bb10cae77
Bas Uitermark <88148607+BasUitermark@users.noreply.github.com>
2023-02-01 18:22:32 +0100

    finished cpp05 started cpp06

---
+++ cpp06/ex00/src/main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: buiterma <buiterma@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/01 18:09:16 by buiterma      #+#    #+#                 */
+/*   Updated: 2023/02/01 18:22:31 by buiterma      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 2)
+		return (EXIT_FAILURE);
+	
+}
